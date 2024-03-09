8d15df2790dfe5dd10e704534b6fb7fb6286f712
akshaydixit2905 <akshaydixit2905@gmail.com>
2024-03-09 12:46:30 +0530

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
+"use client"; 
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { SyncLoader } from "react-spinners";
 
 export default function Home() {
+
+  const [name, setName] = useState("")
+  const [disableButton, setDisableButton] = useState(true)
+  const [age, setAge] = useState("")
+  const [gender, setGender] = useState("")
+  const [countryCode, setCountryCode] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if(name){
+      setDisableButton(false)
+    }
+    else{
+      setDisableButton(true)
+    }
+  },[name])
+
+  async function ageApi() {
+    setLoading(true)
+    setError(false)
+    let url = `https://api.agify.io/?name=${encodeURIComponent(name)}`
+
+    fetch(url, {method: 'GET'})
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setAge(data.age)
+      genderApi()
+    } )
+    .catch(() => {
+      setLoading(false)
+      setName("")
+      setError(true)
+    })
+  }
+
+  async function genderApi() {
+    let url = `https://api.genderize.io/?name=${encodeURIComponent(name)}`
+
+    fetch(url, {method: 'GET'})
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setGender(data.gender)
+      countryApi()
+    } )
+    .catch(() => {
+      setLoading(false)
+      setName("")
+      setError(true)
+    })
+  }
+
+  async function countryApi() {
+    let url = `https://api.nationalize.io/?name=${encodeURIComponent(name)}`
+
+    fetch(url, {method: 'GET'})
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let countryConfig = new Intl.DisplayNames(['en'], {type: 'region'})
+      let countryName:string = countryConfig.of(data.country[0].country_id)!
+      setCountryCode(countryName)
+      setLoading(false)
+    } )
+    .catch(() => {
+      setLoading(false)
+      setName("")
+      setError(true)
+    })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col bg-white items-center">
+      <Image
+      src="/Hy_Vee_Logo.jpg"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <input name="name" placeholder="Search Name..." className="flex w-2/4 bg-slate-100 py-4 rounded-lg px-4 text-black text-lg font-sans" onChange={(val) => setName(val.target.value)} value={name} />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    <button className={`flex bg-red-600 mt-6 max-w-[250px] w-full justify-center items-center rounded-lg h-14 ${disableButton ? 'opacity-60' : 'opacity-100'}`} onClick={() => disableButton ? null : ageApi()}>
+      {loading ?
+            <SyncLoader color="#FFFFFF" size={12} />
+            : 
+            <text className="text-lg font-sans">Search</text>
+      }
+    </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    {!loading? <div className="flex flex-row justify-evenly w-2/4 mt-6" >
+      {age ?<div className="flex flex-col items-center justify-center w-52 h-32 border border-gray-300 rounded-lg" >
+        <text className="text-base font-sans text-black font-bold">Age</text>
+        <text className="text-lg font-sans text-black">{age}</text>
+      </div> : null}
+      {gender ? <div className="flex flex-col items-center justify-center w-52 h-32 border border-gray-300 rounded-lg" >
+        <text className="text-base font-sans text-black font-bold">Gender</text>
+        <text className="text-lg font-sans text-black">{gender}</text>
+      </div> : null}
+      {countryCode ? <div className="flex flex-col items-center justify-center w-52 h-32 border border-gray-300 rounded-lg" >
+        <text className="text-base font-sans text-black font-bold">Country</text>
+        <text className="text-lg font-sans text-black">{countryCode}</text>
+      </div> : null}
+    </div>
+    :null
+    }
+    {error && !loading ? <text className="text-lg font-sans text-black font-bold mt-6">Error!!</text> : null}
     </main>
   );
 }
